24c6c567a5deed5ea3f6e9ba01085e185349bc46
ivanrakic94 <irakic1@gmail.com>
2015-03-23 16:54:05 +0100

    Dodate su metode hashCode, equals i toString u klase Valuta i Kurs. Metoda u klasi valuta je prepravljena tako da poredi samo naziv valute.

--- src/valute/Valuta.java
+++ src/valute/Valuta.java
@@ -20,5 +20,36 @@ public class Valuta {
 		this.skraceniNaziv = skraceniNaziv;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result
+				+ ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valute [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv
+				+ ", kursevi=" + kursevi + "]";
+	}
 
 }
